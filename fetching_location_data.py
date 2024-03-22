39555c524bddf000b325ca1f34286e33e7119b2c
“Pawel” <pawel.gozdur@atos.net>
2024-03-22 13:25:25 +0100

    sdf

---
+++ fetching_location_data.py
@@ -0,0 +1,24 @@
+import os
+import requests
+
+BASE_URL = os.getenv('NAUTOBOT_URL')
+TOKEN = os.getenv('TOKEN')
+LOCATIONS_ENDPOINT = '/api/dcim/locations/'
+
+
+HEADERS = {
+   'Authorization': f'Token {TOKEN}',
+   'Content-Type': 'application/json',
+   'Accept': 'application/json',
+}
+
+full_url = f"{BASE_URL}{LOCATIONS_ENDPOINT}"
+
+response = requests.get(full_url, headers=HEADERS)
+
+if response.status_code == 200:
+   locations = response.json()
+   for location in locations['results']:
+       print(f"Name: {location['name']}")
+else:
+   print(f"Failed to fetch locations: {response.status_code}")
