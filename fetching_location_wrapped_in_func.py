c066f429faa2c3bc55ba99356925af5c54c73593
“Pawel” <pawel.gozdur@atos.net>
2024-03-22 13:29:35 +0100

    sd

---
+++ fetching_location_wrapped_in_func.py
@@ -0,0 +1,27 @@
+import os
+import requests
+
+BASE_URL = os.getenv('NAUTOBOT_URL')
+TOKEN = os.getenv('TOKEN')
+LOCATIONS_ENDPOINT = '/api/dcim/locations/'
+
+
+HEADERS = {
+   'Authorization': f'Token {TOKEN}',
+   'Content-Type': 'application/json',
+   'Accept': 'application/json',
+}
+
+
+def display_locations(locations_url):
+   response = requests.get(locations_url, headers=HEADERS)
+   if response.status_code == 200:
+       locations = response.json()
+       for location in locations['results']:
+           print(f"Name: {location['name']}")
+   else:
+       print(f"Failed to fetch locations: {response.status_code}")
+
+
+locations_url = f"{BASE_URL}{LOCATIONS_ENDPOINT}"
+display_locations(locations_url)
